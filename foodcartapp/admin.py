8e5564cb898b2a3cb05e82ca77c1759f74011af5
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-10-14 23:59:09 +0300

    Add redirect

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
+
 
 from .models import Product, Order, OrderItem
 from .models import ProductCategory
@@ -127,3 +129,10 @@ class OrdertAdmin(admin.ModelAdmin):
     inlines = [
         OrderItemInline
     ]
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        if "next" in request.GET:
+            return HttpResponseRedirect(request.GET['next'])
+        else:
+            return res
