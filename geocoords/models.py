dd3a95726d13a44c75a1fdb5b3236a1adb405023
OlgaZhivaeva <olga.zhiv@yandex.ru>
2024-10-20 19:55:35 +0300

    Save distance

---
+++ geocoords/models.py
@@ -0,0 +1,35 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+
+class Geocoords(models.Model):
+    address = models.CharField(
+        verbose_name='адрес',
+        max_length=200,
+        blank=True,
+        unique=True
+    )
+    lon = models.DecimalField(
+        verbose_name='долгота',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+        blank=True
+    )
+    lat = models.DecimalField(
+        verbose_name='широта',
+        max_digits=8,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+        blank=True
+    )
+    request_date = models.DateTimeField(
+        verbose_name='дата запроса',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'координаты'
+        verbose_name_plural = 'координаты'
+
+    def __str__(self):
+        return self.address
