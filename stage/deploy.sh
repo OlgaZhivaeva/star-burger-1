a2c1dcc08362fff5bb08491cdde1134070470514
OlgaZhivaeva <olga.zhiv@yandex.ru>
2025-03-01 00:10:43 +0300

    Fix stage/deploy.sh

--- stage/deploy.sh
+++ stage/deploy.sh
@@ -4,8 +4,12 @@ set -e
 
 cd ~/opt/star-burger-1/stage
 
+echo "Обновление кода из GitHub..."
+git pull origin master
+
 echo "Остановка старых контейнеров..."
 docker compose down
+docker system prune -a --volumes -f
 
 echo "Сборка образов..."
 docker compose build
