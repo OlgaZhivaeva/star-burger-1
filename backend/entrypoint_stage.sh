51047e1b51117d7b13f5a0f44ab03bc869246607
OlgaZhivaeva <olga.zhiv@yandex.ru>
2025-03-07 19:44:24 +0300

    Delete waiting, add condition:

--- backend/entrypoint_stage.sh
+++ backend/entrypoint_stage.sh
@@ -2,10 +2,6 @@ echo "Start makemigrations..."
 python manage.py makemigrations --dry-run --check &&
 echo "Start migrate..."
 python manage.py migrate --noinput &&
-while [ ! -f /star-burger/frontend/bundles-ready.flag ]; do
-  echo "Waiting for frontend to be ready..."
-  sleep 1
-done
 echo "Start collectstatic..."
 python manage.py collectstatic --noinput &&
 echo "Start server..."
